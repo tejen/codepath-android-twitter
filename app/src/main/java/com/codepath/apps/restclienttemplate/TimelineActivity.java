8126bb8187c912b2a742a24bde6600c4a1098844
Tejen Patel <github@tejen.net>
2017-06-06 16:55:53 -0700

    Create and connect TweetAdapter to List

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,18 +2,27 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
 
     private TwitterClient client;
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,23 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
         client = TwitterApp.getRestClient();
+
+        // find the RecyclerView
+        rvTweets = (RecyclerView) findViewById(R.id.rvTweet);
+
+        // init the arraylist (data source)
+        tweets = new ArrayList<>();
+
+
+        // construct the adapter from this datasource
+        tweetAdapter = new TweetAdapter(tweets);
+
+        // RecyclerView setup (layout manager, use adapter)
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+
+        // set the adapter
+        rvTweets.setAdapter(tweetAdapter);
+
         populateTimeline();
     }
 
@@ -33,7 +59,19 @@ public class TimelineActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("TwitterClient", response.toString());
+//                Log.d("TwitterClient", response.toString());
+                // iterate through array
+                // for each, deserialize JSON object and convert to Tweet object
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(tweets.size());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,79 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+/**
+ * Created by tejen on 6/6/17.
+ */
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+
+    private List<Tweet> mTweets;
+    Context context;
+
+    // pass in the Tweets array in the constructor
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+
+    // for each row, inflate the layout and cache references into ViewHolder
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+    // bind the values based on the position of the element
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // get the data according to position
+        Tweet tweet = mTweets.get(position);
+
+        // populate the views according to this data
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    // create ViewHolder class
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            // perform findViewById lookups
+
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUserName);
+            tvBody= (TextView) itemView.findViewById(R.id.tvBody);
+        }
+    }
+
+}
