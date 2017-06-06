3018b865b19569936dd0a59186cc41ff9c4d4255
Tejen Patel <github@tejen.net>
2017-06-06 16:36:21 -0700

    Create Tweet data model

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,32 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by tejen on 6/6/17.
+ */
+
+public class Tweet {
+
+    // list out the attributes
+    public String body;
+    public long uid; // database ID for the tweet
+    public User user;
+    public String createdAt;
+
+    // deserialize JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException{
+        Tweet tweet = new Tweet();
+
+        // extract the values from JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+
+        return tweet;
+    }
+
+
+}
