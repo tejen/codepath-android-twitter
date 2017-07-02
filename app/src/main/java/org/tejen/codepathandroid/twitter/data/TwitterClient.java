a9b3722d7d5f87b1e5dc24890580db99b65f1a1c
Tejen Patel <github@tejen.net>
2017-07-02 14:26:59 -0700

    Move API Key to secrets.xml

--- app/src/main/java/org/tejen/codepathandroid/twitter/data/TwitterClient.java
+++ app/src/main/java/org/tejen/codepathandroid/twitter/data/TwitterClient.java
@@ -24,10 +24,10 @@ import org.tejen.codepathandroid.twitter.R;
  * 
  */
 public class TwitterClient extends OAuthBaseClient {
-	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance(); // Change this
-	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "U04FBOTu0NpvIxZnxehbXjqE3";       // Change this
-	public static final String REST_CONSUMER_SECRET = "cT8OrcKsG3kXTC8QLYXmwm1iUECvVqc0VHnq8NhUAMpytm7Uh8"; // Change this
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+	public static final String REST_URL = "https://api.twitter.com/1.1";
+	public static String REST_CONSUMER_KEY;
+	public static String REST_CONSUMER_SECRET;
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
@@ -38,8 +38,8 @@ public class TwitterClient extends OAuthBaseClient {
 	public TwitterClient(Context context) {
 		super(context, REST_API_INSTANCE,
 				REST_URL,
-				REST_CONSUMER_KEY,
-				REST_CONSUMER_SECRET,
+				context.getString(R.string.twitter_api_key),
+				context.getString(R.string.twitter_api_secret),
 				String.format(REST_CALLBACK_URL_TEMPLATE, context.getString(R.string.intent_host),
 						context.getString(R.string.intent_scheme), context.getPackageName(), FALLBACK_URL));
 	}
